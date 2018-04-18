d48c498d027bfb3a101edccb179dede343a2ba89
sveto <svetline.kuz@gmail.com>
2018-04-18 19:21:41 +0400

    Сделана заготовка для сервера

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -1,5 +1,9 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -7,8 +11,46 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Main {
-    public static void main(String[] args) throws RemoteException, AlreadyBoundException {
-        try {
+    public static void main(String[] args) {
+        try(ServerSocket server = new ServerSocket(1025)) {
+            Socket client = server.accept();
+            System.out.println("Соединение установлено.");
+
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            DataInputStream in = new DataInputStream(client.getInputStream());
+
+            while (!client.isClosed()) {
+                String request = in.readUTF();
+                String[] splitRequest = request.split(" ");
+                //проверка запроса
+                int first = Integer.parseInt(splitRequest[0]);
+                int second = Integer.parseInt(splitRequest[1]);
+                String response;
+                if(second != 0) {
+                    int div = first / second;
+                    int mod = first % second;
+
+                    response = "Результат целочисленного деления = " + div + ", остаток от деления = " + mod;
+                }
+                else{
+                    response = "Деление на ноль!";
+                }
+
+                out.writeUTF(response);
+                out.flush();
+                if(request.indexOf("end")>0){
+                    break;
+                }
+            }
+
+            in.close();
+            out.close();
+            client.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        /*try {
             Game game = new Game();
             GameI stub = (GameI) UnicastRemoteObject.exportObject(game, 0);
             Registry registry = LocateRegistry.createRegistry(1080);
@@ -17,7 +59,7 @@ public class Main {
         } catch (Exception e)
         {
             e.printStackTrace();
-        }
+        }*/
 
     }
 }
